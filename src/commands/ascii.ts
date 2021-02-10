3a5f9d37924761019facaf3a6a7edea0e69b8722
aRandomGithubUser <aRandomGithubUser@github.com>
2021-02-10 21:29:17 +1100

    added ascii command

--- src/commands/ascii.ts
+++ src/commands/ascii.ts
@@ -2,16 +2,15 @@ import { Client, Message } from "discord.js";
 import figlet from "figlet";
 
 export function run(client: Client, message: Message, args: Array<String>) {
-  args.shift()
 
   let text = args.join(' ')
-  console.log(text)
+  
   figlet(text, (err, data) => {
     if (err) {
       console.error(err)
       return
     }
-    console.log(data)
+    
     message.channel.send(`\`\`\`\n${data}\n\`\`\``)
   })
 }
