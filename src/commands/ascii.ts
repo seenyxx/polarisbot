4b9ca328034d6179155b9e225ea9e97be714186c
aRandomGithubUser <aRandomGithubUser@github.com>
2021-02-10 21:25:59 +1100

    asd

--- src/commands/ascii.ts
+++ src/commands/ascii.ts
@@ -5,12 +5,13 @@ export function run(client: Client, message: Message, args: Array<String>) {
   args.shift()
 
   let text = args.join(' ')
-
+  console.log(text)
   figlet(text, (err, data) => {
     if (err) {
       console.error(err)
       return
     }
+    console.log(data)
     message.channel.send(`\`\`\`\n${data}\n\`\`\``)
   })
 }
