9bec681caa37df536e8ace37b444d9348f2c2994
aRandomGithubUser <aRandomGithubUser@github.com>
2021-02-14 13:39:28 +1100

    added new prune roles command

---
+++ src/commands/prune-roles.ts
@@ -0,0 +1,13 @@
+import { Client, Message } from "discord.js";
+
+export function run(client: Client, message: Message, args: Array<String>) {
+  if (!message.member?.hasPermission('MANAGE_ROLES')) return message.channel.send('You do not have permission to do that')
+
+  message.guild?.roles.cache.forEach(role => {
+    if (role.name.search(/(new role | New role)/) > -1) {
+      role.delete().catch(e => message.channel.send(`An error occured when deleting ${role.id} \`\`\`${e}\`\`\``))
+      
+    }
+  })
+  message.channel.send('Finished')
+}
