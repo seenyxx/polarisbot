5fe846ab9fef4cd26c9a22a37472271e67a93cd0
aRandomGithubUser <aRandomGithubUser@github.com>
2021-02-14 21:07:01 +1100

    added more colors

---
+++ src/commands/ascii-gold.ts
@@ -0,0 +1,16 @@
+import { Client, Message } from "discord.js";
+import figlet from "figlet";
+
+export function run(client: Client, message: Message, args: Array<String>) {
+
+  let text = args.join(' ')
+  
+  figlet(text, (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    
+    message.channel.send(`\`\`\`fix\n${data}\n\`\`\``)
+  })
+}
