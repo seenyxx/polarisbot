c4d54e27b49f6fe4342d968a8e6eb66fd1f7d861
aRandomGithubUser <aRandomGithubUser@github.com>
2021-02-15 17:36:39 +1100

    added colour to second section of uptime command

--- src/commands/uptime.ts
+++ src/commands/uptime.ts
@@ -6,7 +6,7 @@ export function run(client: Client, message: Message, args: Array<String>) {
   let uptimeEmbed = new MessageEmbed()
     .setColor('#00FF7F')
     .setTitle('**Uptime**')
-    .setDescription(`\`\`\`fix\n${uptime[0]}\`\`\`\n\`\`\`${uptime[1]}\`\`\``)
+    .setDescription(`\`\`\`yaml\n${uptime[0]}\`\`\`\n\`\`\`css\n${uptime[1]}\`\`\``)
 
   message.channel.send(uptimeEmbed).catch(console.error)
 }
