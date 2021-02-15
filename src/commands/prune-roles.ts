a92f38aa3790cbfe582d928da083ce15081bccb0
aRandomGithubUser <aRandomGithubUser@github.com>
2021-02-15 17:58:36 +1100

    fixed the prune roles command

--- src/commands/prune-roles.ts
+++ src/commands/prune-roles.ts
@@ -8,7 +8,7 @@ export function run(client: Client, message: Message, args: Array<String>) {
 
   message.guild?.roles.cache.forEach(role => {
     if (role.name.search(/new role/g) > -1 ) {
-      role.delete().catch(e => message.channel.send(`An error occured when deleting ${role.id} \`\`\`${e}\`\`\``))
+      role.delete().catch(e => message.channel.send(simpleEmbed('red','Error',`An error occured when deleting ${role.id} \`\`\`${e}\`\`\``)))
     }
   })
   message.channel.send(simpleEmbed('green', '**Finished pruning roles**', ''))
