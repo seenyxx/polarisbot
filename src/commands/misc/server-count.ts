1423f48804f557c61e8f06dee277ddefbd1fc72c
aRandomGithubUser <aRandomGithubUser@github.com>
2021-02-22 19:06:08 +1100

    added server count command

---
+++ src/commands/misc/server-count.ts
@@ -0,0 +1,12 @@
+import { Client, Message, MessageAttachment } from "discord.js";
+import { checkCoolDown, coolDownSetup, setCoolDown, simpleEmbed, replaceValue } from '../../lib';
+import { BotCache } from '../../cache';
+import nodeHtmlToImage from "node-html-to-image";
+
+let coolDown = 3
+let commandName = 'ping'
+export async function run(client: Client, message: Message, args: Array<string>) {
+  
+  if (coolDownSetup(message, commandName, coolDown)) return
+  message.channel.send(simpleEmbed('blue', 'Server count', `👑 Currently serving ${client.guilds.cache.size} servers`))
+}
