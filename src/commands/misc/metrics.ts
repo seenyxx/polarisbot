4ef6cb76700b8db7a1074df09f23b226f9f0e0ff
aRandomGithubUser <aRandomGithubUser@github.com>
2021-03-13 16:48:28 +1100

    added metrics command

---
+++ src/commands/misc/metrics.ts
@@ -0,0 +1,33 @@
+import { Client, Message } from 'discord.js';
+import { freemem, totalmem } from 'os';
+import { cpuUsage } from 'os-utils';
+
+import { coolDownSetup, simpleEmbed } from '../../util/lib';
+
+let coolDown = 10
+let commandName = 'metrics'
+export async function run(client: Client, message: Message, args: Array<string>) {
+  
+  if (coolDownSetup(message, commandName, coolDown)) return
+  
+  const embed = simpleEmbed('blue', 'Metrics 📊', '')
+  
+  embed.addField('Process CPU Usage', `\`${getCpuUsage()}%\``, true)
+  embed.addField('Process Memory Usage', `\`${heapInMB(process.memoryUsage().heapUsed)}MB\``, true)
+  embed.addField('System Memory Usage', `\`${heapInMB(totalmem()) - heapInMB(freemem())}MB / ${heapInMB(totalmem())}MB\``, true)
+
+  message.channel.send(embed)
+}
+
+function heapInMB(memory: number) {
+  return Math.floor(memory / (1000 * 1000))
+}
+
+function getCpuUsage() {
+  let percentage = 0
+  cpuUsage(p => {
+    percentage = p
+  })
+
+  return percentage
+}
