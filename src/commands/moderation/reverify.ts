f21218b13d1d8f002f6bea9e50026426158d6b17
aRandomGithubUser <aRandomGithubUser@github.com>
2021-03-19 22:50:10 +1100

    fixed reverify

--- src/commands/moderation/reverify.ts
+++ src/commands/moderation/reverify.ts
@@ -1,6 +1,6 @@
 import { Client, Message } from "discord.js";
 import { runCaptcha } from "../../util/captcha";
-import { coolDownSetup, hardPunish, errorMessage, simpleEmbed } from '../../util/lib';
+import { coolDownSetup, hardPunish, errorMessage, simpleEmbed, unverifiedRole } from '../../util/lib';
 
 let coolDown = 3
 let commandName = 'kick'
@@ -19,11 +19,19 @@ export function run(client: Client, message: Message, args: Array<string>) {
   
   member.createDM().then(async dm => {
     await dm.send(simpleEmbed('blue', 'Type here within 10 minutes to start your verification process', ''))
+
+    let unverifiedMemberRole = await unverifiedRole(member.guild)
+    member.roles.add(unverifiedMemberRole)
+
     dm.awaitMessages(m => m, { max: 1, time: 600000}).then(ms => {
       if (ms.first()) {
         message.channel.send(simpleEmbed('green', 'User responded, asking to verify now..', ''))
 
         runCaptcha(member).catch(e => message.channel.send(simpleEmbed('blue', 'Failed verification', e)))
+      }
+
+      if (!ms) {
+        message.channel.send(simpleEmbed('blue', 'User did not respond.', ''))
       }
     })
   })
